2182f74b06f9b9c66b917246de8f75aa2c5e4683
Nikhil Nimbalkar <nsv.nimbalkar@gmail.com>
2020-12-03 19:17:02 +0530

    Initial Deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+// import './Default.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home'
+import Error from './components/Error'
+import Nav from './starWars/Nav'
+import Vehicle from './starWars/Vehicle'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Awesome Star Wars Vehicles!</h1>
+        <Router>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+          <hr />
+          <Nav />
+          <hr />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route path="/vehicle/:vehicleId" component={Vehicle} />
+
+            <Route component={Error} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;

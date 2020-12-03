ecbc86131c50228bcf0400bced4d00209056e1a8
Nikhil Nimbalkar <nsv.nimbalkar@gmail.com>
2020-12-03 19:21:42 +0530

    Second Deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Awesome Star Wars Vehicles!</h1>
+        <h1>My Awesome Star Wars Vehicles!</h1>
         <Router>
           <ul>
             <li><Link to="/">Home</Link></li>
